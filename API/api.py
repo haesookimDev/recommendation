62f0966245cfe951e31f022282ac5fe4b487a78f
ww232330 <ww232330@gmail.com>
2024-07-16 18:37:08 +0900

    refac: refactoring by test code

--- API/api.py
+++ API/api.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from DownloadModel import download_model
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 import mlflow
 import pandas as pd
 from fastapi import FastAPI
-from schemas import PredictIn, PredictOut
+from Schemas import PredictIn, PredictOut
 
 def get_model():
     client = mlflow.tracking.MlflowClient()
@@ -44,10 +44,44 @@ dataloader = DataFetchandLoad(TMA, TA, VAI)
 
 PRE_TMA, PRE_TA, PRE_VAI = dataloader.fetch()
 
+# 데이터 로드
+print("Data Loading")
+
+data = dataloader.load(PRE_TMA, PRE_TA, PRE_VAI)
+
 traveler_len = len(PRE_TMA)
+
+print("Model Testing")
+MODEL.eval()
+with torch.no_grad():
+    _, out = MODEL(data.x, data.edge_index)
+
+    loss_fn = nn.MSELoss()
+
+    loss = loss_fn(out[data.mask], data.y[data.mask])
+    
+print("loss :", loss)
 
 embedding_cache = EmbeddingCache()
 embedding_cache.cache = get_embedding()
+
+def find_next_dest(traveler_id, trip_id):
+    print("Dest Finding")
+    MODEL.eval()
+    with torch.no_grad():
+        cached_embeddings = embedding_cache.get('embeddings')
+
+        traveler_embedding = MODEL.linear(cached_embeddings[traveler_id])
+        trip_embedding = MODEL.linear(cached_embeddings[traveler_len + trip_id])
+
+        # 모든 여행지와의 유사도 계산
+        destination_embeddings = MODEL.linear(cached_embeddings[data.mask])
+        similarities = F.cosine_similarity(traveler_embedding + trip_embedding, destination_embeddings)
+
+        # 가장 유사한 여행지 선택
+        next_destination_id = similarities.argmax().item()
+        predicted_scores = MODEL.linear(cached_embeddings[data.mask][next_destination_id])
+    return next_destination_id, predicted_scores
 
 # Create a FastAPI instance
 app = FastAPI()
@@ -62,19 +96,9 @@ def predict(data: PredictIn) -> PredictOut:
         traveler_id = similarities.content_based_similarity_traveler(PRE_TMA)
     if df['trip_id'] != None:
         trip_id = similarities.content_based_similarity_trip(PRE_TA)
-    MODEL.eval()
-    with torch.no_grad():
-        cached_embeddings = embedding_cache.get('embeddings')
-
-        traveler_embedding = MODEL.linear(cached_embeddings[traveler_id])    
-        trip_embedding = MODEL.linear(cached_embeddings[traveler_len + trip_id])
-
-        # 모든 여행지와의 유사도 계산
-        destination_embeddings = MODEL.linear(cached_embeddings[data.mask])
-        similarities = F.cosine_similarity(traveler_embedding + trip_embedding, destination_embeddings)
-
-        # 가장 유사한 여행지 선택
-        next_destination_id = similarities.argmax().item()
-        predicted_scores = MODEL.linear(cached_embeddings[data.mask][next_destination_id])
+    
+    next_destination_id, predicted_scores = find_next_dest(traveler_id, trip_id)
+    print(f"Similar traveler: {traveler_id},\n{PRE_TMA.iloc[traveler_id].squeeze()}")
+    print(f"Similar trip: {trip_id}, \n{PRE_TA.iloc[trip_id].squeeze()}")
         
     return PredictOut(next_destination_id=next_destination_id, predicted_rating=predicted_scores[0], predicted_recommend=predicted_scores[1], predicted_revisit=predicted_scores[2])
